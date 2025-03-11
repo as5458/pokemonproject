d0b556709a53849ebb9b160929be869103f6788f
as5458 <159526681+as5458@users.noreply.github.com>
2025-03-11 18:57:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+async function fetchdata(){
+    try{
+        const pokemonname=document.getElementById("pokemonname").value.toLowerCase();
+
+
+
+        const response=await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonname}`);
+        if(!response.ok){
+            throw new Error("could not fetch resource")
+        }
+        const data=await response.json()
+        const pokemonsprite=data.sprites.front_default;
+        const imgelement=document.getElementById("pokemon");
+        imgelement.src=pokemonsprite;
+        imgelement.style.display="block";
+      
+        }
+        catch(error)
+       {
+        console.error(error);
+        
+            
+        }
+
+}
